6bff50924e8512169ec5534204cc18a72e137fdd
developermypos <mypos.europe@gmail.com>
2020-07-24 10:43:45 +0300

    update version source

--- IPC/Defines.php
+++ IPC/Defines.php
@@ -35,7 +35,7 @@ class Defines
     const STATUS_TRANSACTION_DECLINED = 21;
     const STATUS_UNDEFINED_ERROR = 99;
     const ENCRYPT_PADDING = OPENSSL_PKCS1_PADDING;
-    const SDK_VERSION = '1.1.0';
+    const SDK_VERSION = '1.1.2';
 
     private function __construct() { }
 }
